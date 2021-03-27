4c61e61cc3a4e75043ffa34957bb80fb52f0fa8a
Nikash Sethi <nikashsethi@gmail.com>
2021-03-27 19:38:27 -0400

    predict logistic regression image encodings (erroring)

--- cub/predicting_encodings.py
+++ cub/predicting_encodings.py
@@ -64,15 +64,15 @@ def get_text_features(dataset_path, seen=True):
 
     vectorizer = TfidfVectorizer(use_idf=True, max_features=1000)
     vectors = vectorizer.fit_transform(corpus)
-    all_features = vectors.todense()
+    all_features = vectors.todense()*100000000
 
     return all_features, all_labels
 
-def main_pickled():
-    train_features, train_labels=pickle.load(open('pickle/train_data.pkl', 'rb'))
-    test_features, test_labels = pickle.load(open('pickle/test_data.pkl', 'rb'))
-    classifier = pickle.load(open('pickle/logres_image_classifier.pkl', 'rb'))
-    return (train_features, train_labels, test_features, test_labels, classifier)
+def accuracy(classifier, features, labels):
+    predictions = classifier.predict(features)
+    print (predictions)
+    accuracy = np.mean((labels == predictions).astype(np.float)) * 100.
+    return accuracy
 
 # images_directory = r'/localtmp/data/cub/CUB_200_2011/images/'
 # train_features, train_labels = get_image_features(images_directory, train=True)
@@ -80,23 +80,39 @@ def main_pickled():
 # test_features, test_labels = get_image_features(directory, train=False)
 # pickle.dump((test_features, test_labels), open('pickle/test_data.pkl', 'wb'))
 
-# classifier = LogisticRegression(C=0.316, max_iter=100, solver='sag', n_jobs=3000)
+train_features, train_labels = pickle.load(open('pickle/train_data.pkl', 'rb'))
+test_features, test_labels = pickle.load(open('pickle/test_data.pkl', 'rb'))
+# classifier = LogisticRegression(C=0.316, max_iter=500, solver='sag', n_jobs=1000, verbose=1)
 # classifier.fit(train_features, train_labels)
 # pickle.dump(classifier, open('pickle/logres_image_classifier.pkl', 'wb'))
-# predictions = classifier.predict(test_features)
-# accuracy = np.mean((test_labels == predictions).astype(np.float)) * 100.
-# print (f'Accuracy: {accuracy:.3f}')
-
-train_features, train_labels, test_features, test_labels, classifier = main_pickled()
+classifier = pickle.load(open('pickle/logres_image_classifier.pkl', 'rb'))
+print (f'Seen accuracy without unseen categories: {accuracy(classifier, test_features, test_labels):.3f}')
 
 wikipedia_directory = r'/localtmp/data/cub/birds_wikipedia/'
 seen_wikipedia_features, seen_wikipedia_labels = get_text_features(wikipedia_directory, seen=True)
 seen_label_indices = np.asarray(seen_wikipedia_labels) - 1
 ordered_seen_wikipedia_features = seen_wikipedia_features[seen_label_indices]
-perceptron = MLPRegressor(max_iter=100, verbose=1, learning_rate='adaptive')
-perceptron.fit(ordered_seen_wikipedia_features, classifier.coef_)
+seen_classifier_intercepts = np.asarray([classifier.intercept_]).T
+seen_image_embeddings = np.concatenate((classifier.coef_, seen_classifier_intercepts), axis=1)
+# perceptron = MLPRegressor(max_iter=1000, learning_rate='adaptive', verbose=1)
+# perceptron.fit(ordered_seen_wikipedia_features, seen_image_embeddings)
+# pickle.dump(perceptron, open('pickle/mlp_wikipedia_regressor.pkl', 'wb'))
+perceptron = pickle.load(open('pickle/mlp_wikipedia_regressor.pkl', 'rb'))
 
+unseen_wikipedia_features, unseen_wikipedia_labels = get_text_features(wikipedia_directory, seen=False)
+unseen_label_indices = np.asarray(unseen_wikipedia_labels) - 161
+ordered_unseen_wikipedia_features = unseen_wikipedia_features[unseen_label_indices]
+unseen_image_embeddings = perceptron.predict(ordered_unseen_wikipedia_features)
+unseen_coefficients = unseen_image_embeddings[:,:-1]
+classifier.classes_ = np.asarray([1+x for x in range(200)])
+classifier.intercept_ = np.concatenate((classifier.intercept_, unseen_image_embeddings[:,-1]))
+classifier.coef_ = np.concatenate((classifier.coef_, unseen_coefficients), axis=0)
 
+print (f'Magnitudes of coefficients: {np.sum(np.abs(classifier.coef_)**2,axis=1)}')
 
+predictions = classifier.predict(test_features)
+print (f'Predictions: {predictions}')
+accuracy = np.mean((test_labels == predictions).astype(np.float)) * 100.
+print (f'Seen accuracy with unseen categories: {accuracy:.3f}')
 
 
